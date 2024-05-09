94a71d031a1ac8347794e259a0360fd6034bb780
Eric Schneider <eric.schneider96@gmail.com>
2024-05-09 13:37:52 +0200

    Move charTensor Code into model
    Allows us to input domain names directly into the model and ensures the
    ordering of char to index mapping to be consistent.

--- model2regex/model.py
+++ model2regex/model.py
@@ -1,6 +1,34 @@
 #!/usr/bin/python
+import torch
+import torch.nn.functional as F
+from torch import Tensor
 from torch import nn
 from string import ascii_lowercase, digits
+from typing import Optional
+from functools import singledispatchmethod
+
+
+class BiMapping:
+    """
+    A bidirectonal mapping class holding two dictionaries
+    used for converting characters to their index and back
+    """
+
+    def __init__(self, characters: str):
+        self._dict = {ch: i for i, ch in enumerate(characters, start=1)}
+        self._reverse = {i: ch for i, ch in enumerate(characters, start=1)}
+
+    @singledispatchmethod
+    def __getitem__(self, item):
+        raise NotImplementedError("item must be of type int or str")
+
+    @__getitem__.register
+    def _(self, item: str) -> int:
+        return self._dict.get(item, 0)
+
+    @__getitem__.register
+    def _(self, item: int) -> str:
+        return self._reverse.get(item, "<END>")
 
 
 class IllegalStartChar(Exception):
@@ -13,18 +41,22 @@ class DGAClassifier(nn.Module):
     classifying DGAs
     """
 
-    vocabular: str = ascii_lowercase + digits + "-."
+    vocabulary: str = ascii_lowercase + digits + "-."
 
-    def __init__(self, emb: int, size: int, nlayers: int, **kwargs):
-        """
-        """
+    # Domains can only be 253 characters long,
+    # adding the start character the max length of our tensors is 254
+    max_len: int = 254
+
+    def __init__(self, emb: int, size: int, nlayers: int, **kwargs) -> None:
         super(DGAClassifier, self).__init__()
-        start_char = kwargs.get("start_char", "_")
-        if start_char in self.vocabular:
-            raise IllegalStartChar("The start character should not be part of the default vocabular")
-        if len(start_char) > 1:
+        self.start_char = kwargs.get("start_char", "_")
+        if self.start_char in self.vocabulary:
+            raise IllegalStartChar("The start character should not be part of the default vocabulary")
+        if len(self.start_char) > 1:
             raise IllegalStartChar("The start character should be of length 1")
-        vocabSize = len(self.vocabular) + len(start_char)
+        self.vocab = self.vocabulary + self.start_char
+        vocabSize = len(self.vocab) + 1
+        self.char2idx = BiMapping(self.vocab)
         self.embedding = nn.Embedding(num_embeddings=vocabSize,
                                       embedding_dim=emb)
         self.rnn = nn.GRU(input_size=emb, hidden_size=size,
@@ -33,7 +65,20 @@ class DGAClassifier(nn.Module):
         self.drop = nn.Dropout(0.3)
         self.sig = nn.Sigmoid()
 
-    def forward(self, input_seq, hidden_state):
+    def charTensor(self, char_seqs: list[str]) -> Tensor:
+        """
+        turn an input sequence of characters into a tensor of indices.
+        """
+        char_seq = map(lambda c: self.start_char + c, char_seqs)
+
+        return torch.stack(tuple(F.pad(torch.tensor([self.char2idx[c] for c in domain]), (0, 254-len(domain))) for domain in char_seq), dim=1)
+
+    def forward(self,
+                input_seq: list[str] | Tensor,
+                hidden_state: Optional[Tensor]) -> tuple[Tensor, Tensor]:
+
+        if isinstance(input_seq, list):
+            input_seq = self.charTensor(input_seq)
         embedding = self.embedding(input_seq)
         output, hidden_state = self.rnn(embedding, hidden_state)
         x = hidden_state[-1, :]
--- model2regex/model_test.py
+++ model2regex/model_test.py
@@ -1,8 +1,12 @@
 from model import IllegalStartChar, DGAClassifier
 import unittest
+import torch
 
 
 class TestModel(unittest.TestCase):
+
+    def setUp(self):
+        self.model: DGAClassifier = DGAClassifier(64, 128, 1)
 
     def test_longer_start_character(self):
         error_msg = "Exception should raise because start_char is longer than 1"
@@ -13,10 +17,42 @@ class TestModel(unittest.TestCase):
 
     def test_start_character_in_vocabulary(self):
         error_msg = "Exception should raise because character already exists in vocabulary"
-        with self.assertRaises(IllegalStartChar,msg=error_msg) as exc:
+        with self.assertRaises(IllegalStartChar, msg=error_msg) as exc:
             DGAClassifier(64, 128, 1, start_char="1")
         exception = exc.exception
-        self.assertEqual(str(exception),"The start character should not be part of the default vocabular")
+        self.assertEqual(str(exception), "The start character should not be part of the default vocabulary")
+
+    def test_charTensor(self):
+        google_tensor = torch.tensor(
+                [[39, 23, 23, 23, 38,  7, 15, 15,  7, 12,  5, 38,  3, 15, 13,  0,  0,  0,
+                 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+                 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+                 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+                 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+                 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+                 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+                 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+                 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+                 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+                 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+                 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+                 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+                 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+                 0,  0]]
+                ).permute(1, 0)
+
+        char_tensor = self.model.charTensor(["www.google.com"])
+        domain = ''.join(self.model.char2idx[c] for c in char_tensor.permute(1,0).squeeze().tolist() if c != 0)
+
+        self.assertEqual(tuple(char_tensor.shape), (254, 1))
+        self.assertTrue(torch.equal(char_tensor, google_tensor))
+        self.assertEqual(domain, "_www.google.com")
+
+    def test_forward_with_string(self):
+        _class, hidden = self.model(["www.google.com", "reddit.com"], None)
+        for item in _class.squeeze().tolist():
+            self.assertTrue(0 < item <= 1)
+        self.assertTupleEqual(tuple(hidden.shape), (1, 2, 128))
 
 
 if __name__ == "__main__":
