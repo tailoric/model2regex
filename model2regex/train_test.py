0a0ee10adee019df30249f223e97e4596dffc842
Eric Schneider <eric.schneider96@gmail.com>
2024-06-13 15:12:34 +0200

    Fix variable initialization order

--- model2regex/train_test.py
+++ model2regex/train_test.py
@@ -8,12 +8,12 @@ class TrainedModelTest(unittest.TestCase):
 
     def setUp(self):
         models_path = pathlib.Path('models')
-        self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-        self.model.to(self.device)
-        self.model.device = self.device
         if not models_path.exists():
             raise unittest.SkipTest("no preset Models found.")
+        self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
         self.model = DGAClassifier(**DEFAULT_MODEL_SETTINGS)
+        self.model.device = self.device
+        self.model.to(self.device)
         self.model.load_state_dict(torch.load(models_path / 'model-fold-2.pth'))
 
     def test_trained_model_input(self):
