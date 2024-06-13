04ff0a674bc0655aa9604515766b0eae67dd8b7d
Eric Schneider <eric.schneider96@gmail.com>
2024-06-13 15:08:02 +0200

    Fix: Set device first when setting up model

--- model2regex/train_test.py
+++ model2regex/train_test.py
@@ -9,12 +9,12 @@ class TrainedModelTest(unittest.TestCase):
     def setUp(self):
         models_path = pathlib.Path('models')
         self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+        self.model.to(self.device)
+        self.model.device = self.device
         if not models_path.exists():
             raise unittest.SkipTest("no preset Models found.")
         self.model = DGAClassifier(**DEFAULT_MODEL_SETTINGS)
         self.model.load_state_dict(torch.load(models_path / 'model-fold-2.pth'))
-        self.model.to(self.device)
-        self.model.device = self.device
 
     def test_trained_model_input(self):
         _ = self.model.predict("www")
