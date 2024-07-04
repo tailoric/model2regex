9a034c52187fff645d88d4e7ff66ea1df1583428
Eric Schneider <eric.schneider96@gmail.com>
2024-07-04 21:20:29 +0200

    Fix regex building of the tree

--- model2regex/regex.py
+++ model2regex/regex.py
@@ -9,6 +9,9 @@ UP = "\x1B[3A"
 CLR = "\x1B[0K"
 
 class Node(TypedDict):
+    """
+    dict struct for typing node information in the DFA
+    """
     item: str
     depth: int
     classification: float | None
@@ -16,7 +19,7 @@ class Node(TypedDict):
 
 class DFA:
     def __init__(self, model: DGAClassifier, store_path = Path("graphs"), root_starter: str = "", threshold: float = 0.4):
-        root_node: Node = {'item': root_starter, 'depth': 0}
+        root_node: Node = {'item': root_starter, 'depth': 0, 'classification': None}
         self.graph = nx.DiGraph()
         self.graph.add_node(0, **root_node)
         self.model = model
@@ -26,7 +29,7 @@ class DFA:
 
     def build_tree(self, store = False) -> None:
         """
-        builds the tree for the DFA, by adding to the DiGraph
+        builds the tree for the DFA, by adding to the DiGraph in self.graph
         """
         char_map = self.model.char2idx
         nodes_to_visit: list[tuple[int,Node]] = [(0,self.graph.nodes[0])]
@@ -35,13 +38,13 @@ class DFA:
         while nodes_to_visit:
             node_id, data = nodes_to_visit.pop(0)
             depth = data.get('depth')
-            starter: list[str|None] = [data.get('item')]
+            root_path_symbols: list[str] = [data.get('item')]
             parent  = list(self.graph.predecessors(node_id))
             while parent:
-                parent_node = self.graph.nodes[parent[0]]
-                starter.append(parent_node.get('item'))
+                parent_node: Node = self.graph.nodes[parent[0]]
+                root_path_symbols.append(parent_node.get('item'))
                 parent: list[int] = list(self.graph.predecessors(parent[0]))
-            starter = "".join(reversed(starter))
+            starter = "".join(reversed(root_path_symbols))
             _, distribution = self.model.predict_next_token(starter)
             mask = distribution.probs > self.threshold
             if torch.any(mask):
@@ -56,54 +59,77 @@ class DFA:
                 if idx != 0:
                     nodes_to_visit.append((new_node_id, new_node))
                 else:
-                    x, _, _ = self.model([starter], None)
+                    x, _, _ = self.model([root_path_symbols], None)
                     new_node['classification'] = x.round().item()
                     end_nodes += 1
                 self.graph.add_node(new_node_id, **new_node)
                 self.graph.add_edge(node_id, new_node_id, probability=round(distribution.probs[idx].item(), ndigits=2))
                 id_counter += 1
 
-            print(f"{UP}nodes to visit: {len(nodes_to_visit):,}, current starter: {starter}{CLR}\n"+
+            print(f"{UP}nodes to visit: {len(nodes_to_visit):,}, current starter: {root_path_symbols}{CLR}\n"+
                   f"tree nodes: {len(self.graph):,}, end nodes: {end_nodes} depth: {depth}{CLR}\n")
 
         if store:
             self.save_file()
 
     def load_file(self, file_path: Path | None):
+        """
+        load file from the file_path provided
+        """
         if not file_path:
             file_path = self.store_path / 'graph.gml.gz'
         self.graph = nx.read_gml(file_path, label='id')
 
     def save_file(self):
+        """
+        save the current dfa as a file called graph.gml.gz at the store path
+        """
         self.store_path.mkdir(exist_ok=True)
         nx.write_gml(self.graph, self.store_path / 'graph.gml.gz')
 
     def visualize_tree(self) -> None:
+        """
+        generate an svg visualization using pydot
+        """
         gp = nx.nx_pydot.to_pydot(self.graph)
-        breakpoint()
+        for node in gp.get_nodes():
+            node.set_label(node.get('item'))
+        for edge in gp.get_edges():
+            edge.set_label(edge.get('probability'))
+
+        gp.write_svg(Path('graphs/tree.svg'))
 
     def build_regex(self) -> str: 
+        """
+        build a regex from the current DFA tree.
+        """
         order = nx.dfs_tree(self.graph, source=0)
+        # a stack of symbols we keep track of so we can apply them whenever we get back out from an end node
         end_symbol_stack = []
         regex_str = ""
+        previous_depth = 0
         for node in order:
             data = self.graph.nodes[node]
             item = data['item']
-            if item == "<END>" and end_symbol_stack:
-                if "".join(end_symbol_stack[-2:]) == "|)":
+            num_child = len(self.graph[node])
+            # we came back out of an end node and need to apply all the closing brackets
+            if previous_depth > data['depth'] and regex_str[-1] == ")":
+                regex_str += end_symbol_stack.pop()
+                # we apply all possible closing brackets and a | to the regex string
+                while end_symbol_stack and regex_str[-1] != "|":
                     regex_str += end_symbol_stack.pop()
+            if item == "<END>" and end_symbol_stack:
                 regex_str += end_symbol_stack.pop()
             else:
                 regex_str += item
-            if len(self.graph[node]) > 1:
-                num_child = len(self.graph[node])
-                end_symbol_stack.extend(")" + "|" * (num_child - 1))
+            if num_child > 1:
                 regex_str += "("
+                end_symbol_stack.extend(")"+ ("|" * (num_child - 1)))
+            previous_depth = data['depth']
         regex_str += "".join(end_symbol_stack)
-        regex_str = regex_str.replace('.', '\\.')
-        print(regex_str)
         return regex_str
-    
+                
+
 
 if __name__ == "__main__":
     model = DGAClassifier(**DEFAULT_MODEL_SETTINGS)
@@ -112,5 +138,5 @@ if __name__ == "__main__":
     dfa = DFA(model, root_starter="", threshold=0.4)
     #dfa.build_tree(store=True)
     dfa.load_file(file_path=Path('graphs/graph.gml.gz'))
-    dfa.build_regex()
+    print(dfa.build_regex())
     dfa.visualize_tree()
