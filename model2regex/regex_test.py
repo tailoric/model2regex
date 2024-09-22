53a955483af311fd4fd7fdb97de304ce507be809
Eric Schneider <eric.schneider96@gmail.com>
2024-09-22 17:25:47 +0200

    Fix tests

--- model2regex/regex_test.py
+++ model2regex/regex_test.py
@@ -29,7 +29,7 @@ class RegexGenTest(unittest.TestCase):
         self.model.device = self.device
         self.model.to(self.device)
         path = pathlib.Path(self.test_directory.name)
-        self.test_dfa = DFA(self.model, heuristic=Threshold(), store_path=path, root_starter="www.google.")
+        self.test_dfa = DFA(self.model, heuristic=Threshold(max_depth=4), store_path=path, root_starter="www.google.")
 
     def test_build_tree(self):
         self.test_dfa.build_tree(store=True)
@@ -37,7 +37,7 @@ class RegexGenTest(unittest.TestCase):
 
     def test_loading_dfa_tree(self):
         self.test_dfa.build_tree()
-        new_dfa = DFA(self.model, heuristic=Threshold(), store_path=pathlib.Path(self.test_directory.name), root_starter="")
+        new_dfa = DFA(self.model, heuristic=Threshold(max_depth=4), store_path=pathlib.Path(self.test_directory.name), root_starter="")
         new_dfa.load_file(new_dfa.store_path / 'graph.gml.gz')
         start_node = new_dfa.graph.nodes[0]
         self.assertEqual(start_node.get('item'), 'www.google.')
@@ -45,71 +45,71 @@ class RegexGenTest(unittest.TestCase):
         
 
     def test_build_regex_max_two_children_max_depth_two(self):
-        simple_dfa = DFA(self.model, heuristic=Threshold(), store_path=pathlib.Path(self.test_directory.name), root_starter="")
+        simple_dfa = DFA(self.model, heuristic=Threshold(max_depth=4), store_path=pathlib.Path(self.test_directory.name), root_starter="")
         simple_dfa.load_file(pathlib.Path("test/test_simple_nodes_two_children.gml"))
         regex = simple_dfa.build_regex()
-        self.assertEqual("a(a|b)|b(a|b)|cc", regex)
+        self.assertEqual("(a(a|b)|b(a|b)|cc)", regex)
 
     def test_build_regex_max_three_children_max_depth_two(self):
-        simple_dfa = DFA(self.model, heuristic=Threshold(), store_path=pathlib.Path(self.test_directory.name), root_starter="")
+        simple_dfa = DFA(self.model, heuristic=Threshold(max_depth=4), store_path=pathlib.Path(self.test_directory.name), root_starter="")
         simple_dfa.load_file(pathlib.Path("test/test_simple_nodes_three_children_depth_two.gml"))
         regex = simple_dfa.build_regex()
-        self.assertEqual("a(a|b|c)|b(a|b)|cc", regex)
+        self.assertEqual("(a(a|b|c)|b(a|b)|cc)", regex)
 
     def test_build_regex_max_two_children_depth_three(self):
-        simple_dfa = DFA(self.model, heuristic=Threshold(), store_path=pathlib.Path(self.test_directory.name), root_starter="")
+        simple_dfa = DFA(self.model, heuristic=Threshold(max_depth=4), store_path=pathlib.Path(self.test_directory.name), root_starter="")
         simple_dfa.load_file(pathlib.Path("test/test_simple_nodes_depth_three.gml"))
         regex = simple_dfa.build_regex()
-        self.assertEqual("a(a(a|b)|b(a|b))|b(a|b)", regex)
+        self.assertEqual("(a(a(a|b)|b(a|b))|b(a|b))", regex)
 
     def test_build_regex_max_two_children_depth_four_center(self):
-        simple_dfa = DFA(self.model, heuristic=Threshold(), store_path=pathlib.Path(self.test_directory.name), root_starter="")
+        simple_dfa = DFA(self.model, heuristic=Threshold(max_depth=4), store_path=pathlib.Path(self.test_directory.name), root_starter="")
         simple_dfa.load_file(pathlib.Path("test/test_simple_nodes_depth_four_middle.gml"))
         regex = simple_dfa.build_regex()
-        self.assertEqual("a(a(a|b)|b(a(a|b)|b))|b(a|b)", regex)
+        self.assertEqual("(a(a(a|b)|b(a(a|b)|b))|b(a|b))", regex)
 
     def test_simplify_regex(self):
-        simple_dfa = DFA(self.model, heuristic=Threshold(), store_path=pathlib.Path(self.test_directory.name), root_starter="")
+        simple_dfa = DFA(self.model, heuristic=Threshold(max_depth=4), store_path=pathlib.Path(self.test_directory.name), root_starter="")
         simple_dfa.load_file(pathlib.Path("test/test_simple_nodes_two_children.gml"))
         simple_dfa.simplify_tree(iterations=3)
         regex = simple_dfa.build_regex()
-        self.assertEqual("[ab][ab]|cc", regex)
+        self.assertEqual("([ab][ab]|cc)", regex)
 
     def test_simplify_regex_max_two_children_depth_four_center(self):
-        simple_dfa = DFA(self.model, heuristic=Threshold(), store_path=pathlib.Path(self.test_directory.name), root_starter="")
+        simple_dfa = DFA(self.model, heuristic=Threshold(max_depth=4), store_path=pathlib.Path(self.test_directory.name), root_starter="")
         simple_dfa.load_file(pathlib.Path("test/test_simple_nodes_depth_four_middle.gml"))
         simple_dfa.simplify_tree(iterations=3)
         regex = simple_dfa.build_regex()
-        self.assertEqual("a(a[ab]|b(a[ab]|b))|b[ab]", regex)
+        self.assertEqual("(a(a[ab]|b(a[ab]|b))|b[ab])", regex)
 
     def test_simplify_regex_max_three_children_max_depth_two(self):
-        simple_dfa = DFA(self.model, heuristic=Threshold(), store_path=pathlib.Path(self.test_directory.name), root_starter="")
+        simple_dfa = DFA(self.model, heuristic=Threshold(max_depth=4), store_path=pathlib.Path(self.test_directory.name), root_starter="")
         simple_dfa.load_file(pathlib.Path("test/test_simple_nodes_three_children_depth_two.gml"))
         simple_dfa.simplify_tree(iterations=3)
         regex = simple_dfa.build_regex()
-        self.assertEqual("a[abc]|b[ab]|cc", regex)
+        self.assertEqual("(a[abc]|b[ab]|cc)", regex)
 
     def test_simplify_regex_max_two_children_depth_three(self):
-        simple_dfa = DFA(self.model, heuristic=Threshold(), store_path=pathlib.Path(self.test_directory.name), root_starter="")
+        simple_dfa = DFA(self.model, heuristic=Threshold(max_depth=4), store_path=pathlib.Path(self.test_directory.name), root_starter="")
         simple_dfa.load_file(pathlib.Path("test/test_simple_nodes_depth_three.gml"))
         simple_dfa.simplify_tree(iterations=3)
         regex = simple_dfa.build_regex()
 
-        self.assertEqual("a[ab][ab]|b[ab]", regex)
+        self.assertEqual("(a[ab][ab]|b[ab])", regex)
     
     def test_simplify_on_abcd_domain_tree(self):
-        dfa = DFA(self.model, heuristic=Threshold(), store_path=pathlib.Path(self.test_directory.name), root_starter="")
+        dfa = DFA(self.model, heuristic=Threshold(max_depth=4), store_path=pathlib.Path(self.test_directory.name), root_starter="")
         dfa.load_file(pathlib.Path('test/abcd_domains.gml.gz'))
         dfa.simplify_tree()
         regex = dfa.build_regex()
-        self.assertEqual("[abcd][abcd][abcd][abcd]", regex)
+        self.assertEqual("([abcd][abcd][abcd][abcd])", regex)
 
     def test_simplify_on_mergeable_nodes_in_center(self):
-        simple_dfa = DFA(self.model, heuristic=Threshold(), store_path=pathlib.Path(self.test_directory.name), root_starter="")
+        simple_dfa = DFA(self.model, heuristic=Threshold(max_depth=4), store_path=pathlib.Path(self.test_directory.name), root_starter="")
         simple_dfa.load_file(pathlib.Path("test/test_simple_nodes_three_children_depth_two_inverted.gml"))
         simple_dfa.simplify_tree(iterations=3)
         regex = simple_dfa.build_regex()
-        self.assertEqual("cc|[ab][ab]", regex)
+        self.assertEqual("(cc|[ab][ab])", regex)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
