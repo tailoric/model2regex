42e8b553d15d48349d7153913f7e77c8e52aaf99
Eric Schneider <eric.schneider96@gmail.com>
2024-12-06 17:16:25 +0100

    Update result.py for box plots

--- results.py
+++ results.py
@@ -1,11 +1,7 @@
 import sqlite3
 import matplotlib.pyplot as plt
-import collections
 import numpy as np
-import re
-from pathlib import Path
-from itertools import groupby
-import time
+import pathlib
 
 def create_graph(results: dict, thresholds: list[float]):
     for domain, sizes in results.items():
@@ -20,21 +16,21 @@ def create_graph(results: dict, thresholds: list[float]):
             y_err = np.asarray(rows['error_tp'])
             ax.plot(thresholds, rows['tp'] , label='true positives', color="tab:blue", linewidth=4)
             ax.fill_between(thresholds, y - y_err, y+ y_err, alpha=0.5)
-            ax.set_title(f"chunk size: {size}", fontsize=20)
+            ax.set_title(f"split size: {size}", fontsize=20)
             ax.set_ylim(bottom=0)
-            ax.set_xlabel("threshold", fontsize=16)
-            ax.set_ylabel("true positvies", fontsize=16)
+            ax.set_xlabel("threshold", fontsize=20)
+            ax.set_ylabel("true positives", fontsize=20)
             ax2 = ax.twinx()
             y = np.asarray(rows['fp'])
             y_err = np.asarray(rows['error_fp'])
             ax2.tick_params(axis='both', which='major', labelsize=20)
-            ax2.set_ylabel("false positives", fontsize=16)
+            ax2.set_ylabel("false positives", fontsize=20)
             ax2.plot(thresholds, rows['fp'] , label='false positives', color="tab:red", linewidth=4)
             ax2.fill_between(thresholds, y - y_err, y+ y_err, alpha=0.5, facecolor="tab:red")
             lines, labels = ax.get_legend_handles_labels()
             lines2, labels2 = ax2.get_legend_handles_labels()
             ax2.legend(lines + lines2, labels + labels2, bbox_to_anchor=(0, -0.2), prop={'size': 16}, loc='lower left')
-            fig.savefig(f"tp_fp_{domain}_{size}.png", bbox_inches="tight")
+            fig.savefig(f"figs/tp_fp_{domain}_{size}.pdf", bbox_inches="tight")
             plt.close()
 
 def show_results():
@@ -44,14 +40,15 @@ def show_results():
         con.execute("select load_extension('./stats.dll')")
         
         cur = con.execute("""
-        select  threshold,
-                domain_name,
-                split_size,
-                avg(true_positives) mean_tp,
-                stddev_pop(true_positives) / sqrt(count(*)) error_tp,
-                avg(false_positives) mean_fp,
-                stddev_pop(false_positives) / sqrt(count(*)) error_fp,
-                with_kl from result
+        select round(threshold, 2) threshold,
+        domain_name,
+        split_size,
+        avg(true_positives) / DGA_total mean_tp,
+        (stddev_pop(true_positives) / sqrt(count(*))) / DGA_total error_tp,
+        avg(false_positives) / real_domain_total mean_fp,
+        (stddev_pop(false_positives) / sqrt(count(*))) / real_domain_total error_fp,
+        regex,
+        with_kl from result
         where with_kl = 0
         group by with_kl, domain_name, threshold, split_size
         ORDER BY split_size
@@ -77,11 +74,6 @@ def show_results():
 domain_regexes = [
         ('generate_url_scheme_1',r'[abc][abc][ab][ab]-domain\.com'),
         ('generate_url_scheme_2',r'[abcd][abcd][abcd][abcd]-domain\.com'),
-        #('simpleExreg', r'[0123456789][0123456789][0123456789]([abc][abc]|[abc][abc][abc]|[abc][abc][abc][abc]|[abc][abc][abc][abc][abc])(|[xyz]|[xyz][xyz]|[xyz][xyz][xyz]|[xyz][xyz][xyz][xyz])\.(evil|knievel)\.com'),
-        #('statefulExreg', (r'[0123456789][0123456789][0123456789][0123456789][0123456789]\.evil\.([abc]|[abc][abc])\.com',
-        #r'[0123456789][0123456789][0123456789][0123456789][0123456789]\.knievel\.([abc][abc][abc]|[abc][abc][abc][abc])\.com')),
-        #('statefulExregSameLength',(r'[0123456789][0123456789][0123456789][0123456789][0123456789]\.evil\.[abc][abc][abc][abc]\.com',
-        #r'[0123456789][0123456789][0123456789]\.knievel\.[abc][abc][abc]\.com'))
         ]
 
 def split_into_atoms(regex: str, size: int):
@@ -113,6 +105,47 @@ def split_into_atoms(regex: str, size: int):
         splits.append(current)
     return splits
  
+def save_box_plot(i:int, con: sqlite3.Connection, domains, labels, file_path: pathlib.Path):
+        build_times = []
+        simplify_times = []
+        fig, (ax_build, ax_simplify) = plt.subplots(1, 2)
+        fig.set_size_inches(16,9)
+        for domain in domains:
+            cur = con.execute('''
+            select domain_name, split_size, round(threshold, 2) threshold, rt.tree_build_time_seconds build_time, rt.simplify_build_time_seconds simplify_time
+            from run_time rt 
+            inner join result r on rt.result_id = r.result_id and with_kl = 0
+            where domain_name = ? and split_size = ?
+            ''', (domain, i))
+            rows = cur.fetchall()
+            build_times.append(list(map(lambda r: r['build_time'], rows)))
+            simplify_times.append(list(map(lambda r: r['simplify_time'], rows)))
+        ax_build.boxplot(build_times, tick_labels=labels)
+        ax_build.set_ylabel('runtime in seconds', fontsize=20)
+        ax_build.tick_params(labelsize=20)
+        ax_build.set_title(f"split size {i} runtime build  tree", fontsize=20)
+        ax_simplify.boxplot(simplify_times,tick_labels=labels)
+        ax_simplify.tick_params(labelsize=20)
+        ax_simplify.set_ylabel('runtime in seconds', fontsize=20)
+        ax_simplify.set_title(f"split size {i} runtime simplify tree", fontsize=20)
+        fig.tight_layout()
+        fig.savefig(file_path)
+        plt.close()
+
+def box_plot_runtime():
+    with sqlite3.connect('results.db') as con:
+        con.enable_load_extension(True)
+        con.row_factory = sqlite3.Row
+        con.execute("select load_extension('./stats.dll')")
+        con.execute("select load_extension('./fuzzy.dll')")
+        con.set_trace_callback(print)
+        for i in range(2, 6):
+            save_box_plot(i, con, ('generate_url_scheme_1', 'generate_url_scheme_2'), ('baseline double block', 'baseline single block'), pathlib.Path(f'figs/baseline_domains_run_times_split_size_{i}.pdf'))
+            save_box_plot(i, con, ('statefulExreg', 'statefulExreg'), ('stateful', 'stateful aligned'), pathlib.Path(f'figs/stateful_domains_runtime_split_size_{i}.pdf'))
+            save_box_plot(i, con, ('banjori',), ('banjori',), pathlib.Path(f'figs/banjori_domains_runtime_split_size_{i}.pdf'))
+            if i != 5:
+                save_box_plot(i, con, ('simpleExreg',), ('full dynamic',), pathlib.Path(f'figs/full_dynamic_runtime_split_size_{i}.pdf'))
+
 
 def regex_show_distances():
     with sqlite3.connect('results.db') as con:
@@ -131,8 +164,8 @@ def regex_show_distances():
                 select 
                     threshold, 
                     split_size, 
-                    avg(true_positives) as true_positives, 
-                    stddev_pop(true_positives)/sqrt(count(*)) as tp_error,
+                    avg(true_positives)/ DGA_total as true_positives, 
+                    (stddev_pop(true_positives)/sqrt(count(*))) / DGA_total as tp_error,
                     regex, 
                     avg(fuzzy_damlev(regex, ?)) as mean_dst,
                     stddev_pop(fuzzy_damlev(regex, ?)) / sqrt(count(*)) as dst_error
@@ -148,23 +181,27 @@ def regex_show_distances():
                 true_positives = np.asarray([r['true_positives'] for r in results])
                 distance = np.asarray([r['mean_dst'] for r in results])
                 error = np.asarray([r['dst_error'] for r in results])
-                ax.set_xlabel('thresholds', fontsize=16)
-                ax.set_ylabel('Damlev-Levenshtein distance', fontsize=16)
+                ax.set_xlabel('thresholds', fontsize=20)
+                ax.set_ylabel('Damlev-Levenshtein distance', fontsize=20)
                 ax.tick_params(axis='both', which='major', labelsize=20)
                 ax2 = ax.twinx()
                 ax.plot(thresholds, distance, label='edit distance', linewidth=4)
-                ax2.plot(thresholds, true_positives, label='true_positives', color="tab:cyan", linewidth=4)
-                ax2.set_ylabel('true positives', fontsize=16)
+                ax2.plot(thresholds, true_positives, label='true positives', color="tab:cyan", linewidth=4)
+                ax2.set_ylabel('true positives', fontsize=20)
                 ax.fill_between(thresholds, distance - error, distance + error, alpha=0.5)
                 ax2.fill_between(thresholds, true_positives - tp_error, true_positives + tp_error, alpha=0.5, color="tab:cyan")
+                ax2.tick_params(axis='both', which='major', labelsize=20)
                 lines, labels = ax.get_legend_handles_labels()
                 lines2, labels2 = ax2.get_legend_handles_labels()
                 ax2.legend(lines + lines2, labels + labels2, bbox_to_anchor=(0,-0.2), loc='lower left', prop={'size': 16})
-                ax.set_title(f"chunk size: {split_size}", fontsize=20)
-                fig.savefig(f'dst_{domain_name}_{split_size}.png', bbox_inches='tight')
+                ax.set_title(f"split size: {split_size}", fontsize=20)
+                fig.savefig(f'figs/dst_{domain_name}_{split_size}.pdf', bbox_inches='tight')
 
 
 
 if __name__ == "__main__":
+    path = pathlib.Path('figs')
+    path.mkdir(exist_ok=True, parents=True)
+    box_plot_runtime()
     show_results()
     regex_show_distances()
